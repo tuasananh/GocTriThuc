43c7a59cdcc3f72856f38cb484e5c9587dd7b127
tuasananh <tuasananh@gmail.com>
2026-04-25 14:45:19 +0700

    feat: add default role upon registration

--- backend/src/main/resources/db/migration/V202604161951_1__snowflake_id_and_users_table.sql
+++ backend/src/main/resources/db/migration/V202604161951_1__snowflake_id_and_users_table.sql
@@ -47,3 +47,17 @@ CREATE TABLE user_providers (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     UNIQUE (provider_name, provider_user_id)
 );
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER tr_users_updated_at BEFORE UPDATE ON users
+FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
+
+CREATE TRIGGER tr_user_providers_updated_at BEFORE UPDATE ON user_providers
+FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
