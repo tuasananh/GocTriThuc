21201c2afb4b3f353d37bd6a44cd9165a21bba91
tuasananh <tuasananh@gmail.com>
2026-04-30 01:32:31 +0700

    feat: implement auth context and provider

---
+++ frontend/src/contexts/AuthContext.ts
@@ -0,0 +1,25 @@
+import { CurrentUser } from '@/entities/CurrentUser';
+import { createContext, useContext } from 'react';
+
+export type AuthContextType =
+  | {
+      isAuthenticated: false;
+      login: (provider: string) => void;
+    }
+  | {
+      isAuthenticated: true;
+      user: CurrentUser;
+      logout: () => Promise<void>;
+    };
+
+const AuthContext = createContext<AuthContextType | null | undefined>(undefined);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
+
+export default AuthContext;
